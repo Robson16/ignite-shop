25e3c8f738c1964c7ebc294bb8f66e852852308b
Robson Henrique Rodrigues <robhenrod@gmail.com>
2026-01-03 15:37:12 -0300

    Estrutura visual - Estrutura da Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
-import { styled } from '@/app/_styles/stitches.config'
+import Image from 'next/image'
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  fontSize: '3rem',
-})
+import shirt1 from './_assets/t-shirts/1.png'
+import shirt2 from './_assets/t-shirts/2.png'
+import shirt3 from './_assets/t-shirts/3.png'
+import { HomeContainer, Product } from './_styles/pages/home'
 
 export default function Home() {
-  return <Button>Hello Shop</Button>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="Shirt 1" />
+
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="Shirt 2" />
+
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
