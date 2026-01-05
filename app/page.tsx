b4e4f7a87cd334132d8bb3296e97785392fce97c
Robson Henrique Rodrigues <robhenrod@gmail.com>
2026-01-05 17:26:28 -0300

    Integração com Stripe - Utilizando SSG

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,32 @@
+import { unstable_cache } from 'next/cache'
 import Stripe from 'stripe'
 
 import { ProductSlider } from './_components/ProductSlider'
 import { stripe } from './_services/stripe'
 import { HomeContainer } from './_styles/pages/home'
 
+const getProducts = unstable_cache(
+  async () => {
+    const response = await stripe.products.list({
+      expand: ['data.default_price'],
+    })
+
+    return response.data.map((product) => {
+      const price = product.default_price as Stripe.Price
+      return {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: price.unit_amount || 0,
+      }
+    })
+  },
+  ['products-list'], // Cache key
+  { revalidate: 60 }, // 60 seconds
+)
+
 export default async function Home() {
-  const response = await stripe.products.list({
-    expand: ['data.default_price'],
-  })
-
-  const products = response.data.map((product) => {
-    const price = product.default_price as Stripe.Price
-
-    return {
-      id: product.id,
-      name: product.name,
-      imageUrl: product.images[0],
-      price: price.unit_amount || 0,
-    }
-  })
+  const products = await getProducts()
 
   return (
     <HomeContainer>
