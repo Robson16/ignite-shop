1c3260d4129a6aa897ba706e91083dc262d9a37a
Robson Henrique Rodrigues <robhenrod@gmail.com>
2026-01-06 13:12:25 -0300

    Produto & Checkout - Caregando dados do produto

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,27 +1,53 @@
-'use client'
-
+import { unstable_cache } from 'next/cache'
 import Image from 'next/image'
-import { useParams } from 'next/navigation'
+import Stripe from 'stripe'
+
+import { stripe } from '@/app/_services/stripe'
 
 import { ImageContainer, ProductContainer, ProductDetails } from './styles'
 
-export default function ProductPage() {
-  const params = useParams()
-  const id = params?.id ?? '—'
+interface ProductPageProps {
+  params: Promise<{ id: string }>
+}
+
+const getProduct = unstable_cache(
+  async (productId: string) => {
+    const product = await stripe.products.retrieve(productId, {
+      expand: ['default_price'],
+    })
+
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format((price.unit_amount || 0) / 100),
+      description: product.description,
+    }
+  },
+  ['product-detail'], // Cache key
+  { revalidate: 3600 }, // 1 hour
+)
+
+export default async function ProductPage({ params }: ProductPageProps) {
+  const { id } = await params
+  const product = await getProduct(id)
 
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo,
-          illo. Alias sequi quidem id officia nemo itaque neque quo rem rerum
-          nam, facilis in ea quas repudiandae quasi explicabo autem!
-        </p>
+        <p>{product.description}</p>
 
         <button>Comprar agora</button>
       </ProductDetails>
