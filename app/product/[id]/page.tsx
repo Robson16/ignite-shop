f28ff7e097f3906ec70dbe33ea6954597de7470c
Robson Henrique Rodrigues <robhenrod@gmail.com>
2026-01-06 12:05:25 -0300

    Produto & Checkout - Estrutura da página de produto

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,10 +1,30 @@
 'use client'
 
+import Image from 'next/image'
 import { useParams } from 'next/navigation'
+
+import { ImageContainer, ProductContainer, ProductDetails } from './styles'
 
 export default function ProductPage() {
   const params = useParams()
   const id = params?.id ?? '—'
 
-  return <h1>Página do Produto: {id}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo,
+          illo. Alias sequi quidem id officia nemo itaque neque quo rem rerum
+          nam, facilis in ea quas repudiandae quasi explicabo autem!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
