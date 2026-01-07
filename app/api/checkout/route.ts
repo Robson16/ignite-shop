a0dfea4596b903118a1ec448108def24ffcf57d0
Robson Henrique Rodrigues <robhenrod@gmail.com>
2026-01-07 12:05:29 -0300

    Produto & Checkout - Dados da compra no sucesso & Redirect no SSR

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
     return NextResponse.json({ error: 'Price ID not found.' }, { status: 400 })
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success`
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
   const cancelUrl = `${process.env.NEXT_URL}/`
 
   const checkoutSession = await stripe.checkout.sessions.create({
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,17 +1,45 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
+import Stripe from 'stripe'
 
+import { stripe } from '../_services/stripe'
 import { ImageContainer, SuccessContainer } from './styles'
 
-export default function Success() {
+interface SuccessPageProps {
+  searchParams: Promise<{ session_id?: string }>
+}
+
+export default async function SuccessPage({ searchParams }: SuccessPageProps) {
+  const { session_id: sessionId } = await searchParams
+
+  if (!sessionId) {
+    redirect('/')
+  }
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  })
+
+  const customerName = session.customer_details?.name
+  const product = session.line_items?.data[0].price?.product as Stripe.Product
+
   return (
     <SuccessContainer>
       <h1>Compra efetuada</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image
+          src={product.images[0]}
+          width={120}
+          height={110}
+          alt={product.name}
+        />
+      </ImageContainer>
 
       <p>
-        Uhuul <strong>John Doe</strong>, sua <strong>Camiseta X</strong> já está
-        a caminho da sua casa.
+        Uhuul <strong>{customerName}</strong>, sua{' '}
+        <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
 
       <Link href="/">Voltar ao catálogo</Link>
